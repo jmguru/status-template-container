dce6fd20c3fa407ff3c73f622b5b1fa906e842b0
John Griffith <jmguru@gmail.com>
2020-03-22 15:52:33 -0500

    Delete post.sh

--- post.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-HERE=$(pwd)
-
-MYSQL_CONTAINER=status-template_dbserver_1
-
-docker cp $HERE/setup.sql $MYSQL_CONTAINER:/
-docker cp $HERE/setup.sh $MYSQL_CONTAINER:/
-docker exec -it $MYSQL_CONTAINER "./setup.sh"
